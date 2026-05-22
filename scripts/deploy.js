7a8ffc45296ff76284b881f020fb5e115f547f1f
garuca <gabrilrc@gmail.com>
2026-05-22 17:47:17 -0300

    chore: convert deploy.js script to ES Modules

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,16 @@
-const ghpages = require('gh-pages');
-const path = require('path');
+import ghpages from 'gh-pages';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const distPath = path.resolve(__dirname, '..', 'dist');
 
 console.log('Dist path:', distPath);
 console.log('Files to deploy:');
-require('fs').readdirSync(distPath).forEach(f => console.log(' -', f));
+fs.readdirSync(distPath).forEach(f => console.log(' -', f));
 
 ghpages.publish(
   distPath,
