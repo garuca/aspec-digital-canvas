43d827b9263f3ae52fc2f09993b6e77456f6d2c4
Gabriel Rufino <gabriel.rufino.carvalho@gmail.com>
2026-06-16 15:40:16 -0300

    feat(school): add missing untracked files for NewSchoolPage

---
+++ src/pages/NewSchoolPage.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import NewSchoolNavigatorSection from "@/components/NewSchoolNavigatorSection";
+import Footer from "@/components/Footer";
+
+const NewSchoolPage = () => {
+  return (
+    <div className="min-h-screen bg-[#030014] text-white">
+      <Header />
+      <NewSchoolNavigatorSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default NewSchoolPage;
