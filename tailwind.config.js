e67f63be7670547621948444f705a7da75ab6ee0
Gabriel Rufino <gabriel.rufino.carvalho@gmail.com>
2026-06-30 10:20:41 -0300

    feat: adiciona página de pré-inscrição do curso Formação Master Swift

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html", "./swift/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
